2f7845b20025b5d4b0376216992e21a14f4b21a4
Fletch <fletchmakesstuff@gmail.com>
2024-03-29 17:46:58 -0400

    initial commit of lua file

---
+++ pterm.lua
@@ -0,0 +1,16 @@
+--[[pod_format="raw",created="2024-03-28 16:28:33",modified="2024-03-28 16:30:21",revision=2]]
+-- pterm.lua
+-- fletch
+-- a helpful utility that acts like a terminal but with helper functions for controlled printing
+
+function _init()
+
+end
+
+function _update()
+
+end
+
+function _draw()
+
+end
